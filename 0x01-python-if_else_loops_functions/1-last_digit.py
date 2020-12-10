c623b97cefc30c626b50d888bd96e8913004bd19
alexnsweet <alexnsweet@gmail.com>
2020-12-10 03:31:07 +0000

    wrong type

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -14,4 +14,4 @@ elif ldig > 5:
 else:
     suffix = "and is less than 6 and not 0"
 
-print("Last digit of {:d} is {:d} {}".format(number, ldig, suffix))
+print("Last digit of {} is {} {}".format(number, ldig, suffix))
