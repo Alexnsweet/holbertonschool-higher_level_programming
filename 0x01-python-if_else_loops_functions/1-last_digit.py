313391ccb47031215d7a97de6c8d1435f99a97f5
alexnsweet <alexnsweet@gmail.com>
2020-12-10 03:27:53 +0000

    formatted and conditional statments

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+if number >= 0:
+    ldig = number % 10
+else:
+    ldig = (number % -10) * -1
+
+if ldig == 0:
+    suffix = "and is 0"
+elif ldig > 5:
+    suffix = "and is greater than 5"
+else:
+    suffix = "and is less than 6 and not 0"
+
+print("Last digit of {:d} is {:d} {}".format(number, ldig, suffix))
