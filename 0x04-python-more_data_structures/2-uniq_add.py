432d34022de4da0988670d84ba4ec55ada7a2a37
Alexander Sweet <alexnsweet@gmail.com>
2020-12-17 22:54:48 +0000

    back to original

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if my_list:
-        number = 0
-        new_list = my_list[:]
-        new_list.sort()
+    number = 0
+    new_list = my_list[:]
+    new_list.sort()
 
-        for i in range(len(new_list)):
-            if new_list[i] != new_list[i - 1]:
-                number += new_list[i]
-        return number
+    for i in range(len(new_list)):
+        if new_list[i] != new_list[i - 1]:
+            number += new_list[i]
+    return number
