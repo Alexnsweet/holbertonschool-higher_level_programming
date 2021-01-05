dce77f4f5470a50499615c6c0128e274e3d8383d
Alexander Sweet <alexnsweet@gmail.com>
2021-01-05 21:59:57 +0000

    square with comments

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    This is an example of private instance attributes
+"""
+class Square:
+    """
+        This class creates a square with instatiation of size
+    """"
+    def __init__(self, size):
+        self.__size = size
