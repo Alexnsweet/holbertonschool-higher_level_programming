35a20b5402d4e3e787b333cfb11a81838190e5b3
Alexander Sweet <alexnsweet@gmail.com>
2021-01-05 22:10:06 +0000

    added indentations to method doc

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,6 +7,9 @@
 class Square:
     """
         This class creates a square with instatiation of size
-    """"
+    """
     def __init__(self, size):
+        """
+        Initializes private attribute size
+        """
         self.__size = size
