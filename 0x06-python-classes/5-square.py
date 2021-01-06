9e1dc6b2674491adc0a27fde6e1e98d34463193a
Alexander Sweet <alexnsweet@gmail.com>
2021-01-06 00:19:18 +0000

    printed hashtags the number of times of size

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+"""
+    This is an example of validation
+"""
+
+
+class Square:
+    """
+        This class creates a square with instatiation of size with validation
+    """
+    def __init__(self, size=0):
+        """
+        Initializes private attribute size
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+            Attribute to retrieve
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+            Setter to initialize value
+        """
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """
+            Method that defines area of square by squaring size
+        """
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """
+            Method that prints ssquare
+        """
+        for i in range(self.__size):
+            print('#' * self.__size)
